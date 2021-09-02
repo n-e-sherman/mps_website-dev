2be7bb99242e0a3d1e7289e1df1a3d1884249e21
Nick Sherman <n.e.sherman@berkeley.edu>
2021-09-02 16:28:00 -0700

    updated look for correlation

--- app.py
+++ app.py
@@ -18,6 +18,19 @@ import base64
 # from PIL import Image
 # import io
 
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# # Database
+# db = SQLAlchemy(app)
+# class Todo(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+#     def __repr__(self):
+#         return '<Task %r>' % self.id
+
+
+
 app = Flask(__name__)
 
 def rescale(zs, vmin = 0, vmax = 1):
@@ -26,6 +39,14 @@ def rescale(zs, vmin = 0, vmax = 1):
     zmax = np.max(_zs)
     res = ( (_zs - zmin) * (vmax - vmin) ) / (zmax - zmin) + vmin
     return res
+
+def get_hash(inputs):
+	shash = ""
+	if inputs['Correlation'] == "true":
+		shash='Correlation_'
+	if inputs['Chebyshev'] == "true":
+		shash='Chebyshev_'
+	return shash
 
 def make_arguments(inputs):
 	res = []
@@ -50,13 +71,14 @@ def make_plot(df):
 	xs = np.arange(1,L+1)
 	cols = [str(x) for x in xs]
 	Icols = ['I'+str(x) for x in xs]
+	xs = np.arange(-int(L/2)+1,int(L/2)+1)
 
 	ts = np.array(df.t.unique())
-	RZs = np.array(df[cols])**2
+	RZs = np.array(df[cols])
 	AZs = np.sqrt(np.array(df[cols])**2 + np.array(df[Icols])**2)
 	if thermal:
 	    scale = 1
-	    zs = rescale(abs(RZs),vmin = 2E-5, vmax = 1)
+	    zs = rescale(abs(RZs),vmin = 2E-3, vmax = 1)
 	else:
 	    scale = 10
 	    zs = rescale(AZs,vmin = 2E-3, vmax = 1)
@@ -66,10 +88,10 @@ def make_plot(df):
 
 
 	fig = Figure()
+	fig.set_size_inches(6, 4.5)
 	ax = fig.subplots()
 	levs = np.logspace(np.log10(scale*np.min(zs)), np.log10(np.max(zs)), 60)
 	cax = ax.contourf(xs, ts, zs, levs, norm=colors.LogNorm(), extend='both')
-
 	ticks = [levs[0],levs[-1]]
 	labels = ['1E-5','1']
 	cbar = fig.colorbar(cax, ticks=ticks)
@@ -78,27 +100,25 @@ def make_plot(df):
 	ax.set_ylabel('$t$',fontsize=14)
 	fig.tight_layout()
 	buf = BytesIO()
-
 	fig.savefig(buf, format="png")
+	fig.savefig('trash.png')
 	data_corr = base64.b64encode(buf.getbuffer()).decode("ascii")
 
 	fig = Figure()
+	fig.set_size_inches(6, 4.5)
 	ax = fig.subplots()
-	ax.plot(ts,abs(autoR),color='k',label='real')
-	ax.plot(ts,abs(autoI),color='b',label='imaginary')
-	ax.plot(ts,abs(autoA),color='r',label='magnitude')
-	ax.set_xscale('log')
-	ax.set_yscale('log')
-	ax.set_xlabel('$t$',fontsize=14)
-	fig.tight_layout()
-	ax.set_ylabel('$|G(x=0,t)|$',fontsize=14)
+	ax.plot(ts,autoR,color='k',label='Real')
+	ax.plot(ts,autoI,color='b',label='Imaginary',ls=':')
+	ax.plot(ts,autoA,color='r',label='Magnitude',ls='--')
+	ax.legend()
 
-	# ax2 = ax.twinx()
-	# ax2.set_ylabel(r'$G(x=0,t)$',fontsize=14)
-	# ax.legend()
+	ax.set_xlabel('$t$',fontsize=14)
+	ax.set_ylabel('$|G(x=0,t)|$',fontsize=14)
+	fig.tight_layout()
+
 	buf = BytesIO()
 	fig.savefig(buf, format="png")
-	fig.savefig('trash.png')
+	fig.savefig('trash2.png')
 	data_auto = base64.b64encode(buf.getbuffer()).decode("ascii")
 
 	# return f"<img src='data:image/png;base64,{data_corr}'/>\n<img src='data:image/png;base64,{data_auto}'/>"
@@ -109,23 +129,33 @@ def run_code(inputs):
 	# Include a save and load system here to save some time. 
 	# DB is the proper way to do this, but for the sake of time 
 	# we ignore this and just use the value of the form.
+	_hash = get_hash(inputs)
+	inputs['save'] = "false"
 	inputs['write'] = "false"
 	inputs['resDir'] = "code/"
 	inputs['Silent'] = "true"
+
 	inputs['SiteSet'] = "SpinHalf"
 	inputs['Model'] = "XXZ"
 	inputs['beta'] = 0
+	inputs['Evolver'] = "Trotter"
 
-	if(inputs['Evolver'] == "TEBD"):
-		inputs['Evolver'] = "Trotter"
-	inputs['swap'] = "true"
-	inputs['MaxIter'] = 5
+	# if(inputs['Evolver'] == "TEBD"):
+	# 	inputs['Evolver'] = "Trotter"
+	# inputs['swap'] = "true"
+	# inputs['MaxIter'] = 5
 
 
 
 	inputs['nSweeps'] = 5
 	sweeps_maxdim = [int(1.0 / float(inputs['nSweeps']) * n * int(inputs['MaxDim'])) for n in range(1, inputs['nSweeps'] + 1)]
 	inputs['sweeps_maxdim'] = str(sweeps_maxdim).strip("[").strip("]").replace(" ", "")
+
+	# inputs["sweeps_mindim"] = "0,0,0"
+	# inputs["sweeps_cutoff"] = "1E-6,1E-9,1E-12"
+	# inputs["sweeps_niter"] = "4,3,2"
+	# inputs["sweeps_noise"] = "1E-7,1E-10,0"
+
 	if "Ground" in inputs['state']:
 		inputs['thermal'] = "false"
 	result = subprocess.run(["code/main"]+make_arguments(inputs),capture_output=True)
@@ -134,19 +164,6 @@ def run_code(inputs):
 	file_name = _file_name.replace("\\n'","")
 	print(file_name)
 	return pd.read_csv(file_name)
-
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-# # Database
-# db = SQLAlchemy(app)
-# class Todo(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.String(200), nullable=False)
-#     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-#     def __repr__(self):
-#         return '<Task %r>' % self.id
-
 
 # setup initial page
 @app.route('/')
@@ -158,6 +175,7 @@ def correlation():
 	if request.method == 'POST':
 		inputs = dict(request.form)
 		inputs['Correlation'] = "true"
+		inputs['Chebyshev'] = "false"
 		url_cor, url_auto = make_plot(run_code(inputs))
 		return render_template('correlation.html', url_cor=url_cor, url_auto=url_auto)
 	else:
@@ -175,57 +193,6 @@ def correlation_image():
 def neutron():
     return render_template('neutron.html')
 
-
-##########################################
-############### TEMP #####################
-##########################################
-
-# @app.route('/', methods=['POST', 'GET'])
-# def index():
-# 	if request.method == 'POST':
-# 		task_content = request.form['content']
-# 		new_task = Todo(content=task_content)
-
-# 		try:
-# 			db.session.add(new_task)
-# 			db.session.commit()
-# 			return redirect('/')
-# 		except:
-# 			return 'There was an issue adding your task'
-
-# 	else:
-# 		tasks = Todo.query.order_by(Todo.date_created).all()
-# 		return render_template('index.html', tasks=tasks)
-
-# # Delete button implementation
-# @app.route('/delete/<int:id>')
-# def delete(id):
-#     task_to_delete = Todo.query.get_or_404(id)
-
-#     try:
-#         db.session.delete(task_to_delete)
-#         db.session.commit()
-#         return redirect('/')
-#     except:
-#         return 'There was a problem deleting that task'
-
-# Update button implementation
-# @app.route('/update/<int:id>', methods=['GET', 'POST'])
-# def update(id):
-#     task = Todo.query.get_or_404(id)
-
-#     if request.method == 'POST':
-#         task.content = request.form['content']
-
-#         try:
-#             db.session.commit()
-#             return redirect('/')
-#         except:
-#             return 'There was an issue updating your task'
-
-#     else:
-#         return render_template('update.html', task=task)
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port='5000')
+    # app.run()
